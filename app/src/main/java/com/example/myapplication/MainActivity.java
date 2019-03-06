3231c6498819ee1ba4daa3388b14560c4bad51aa
Stoyan <slownessbg@gmail.com>
2019-03-06 21:40:04 +0200

    Adding timestamp

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,29 +6,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView greetingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.greetingView = findViewById(R.id.greeting);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_bye) {
-            this.greetingView.setText(R.string.bye);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+        TextView timeView = findViewById(R.id.textView4);
+        String timeStamp = String.valueOf(java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
+        timeView.setText(timeStamp);
     }
 }
